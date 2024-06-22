3c3f273f28edcc6215da8d536ae778cf5f1908a5
pedro-h-o-soares <pedrosoares77744@gmail.com>
2024-06-22 20:08:17 -0300

    Create Services

---
+++ CellManagerAPI.Domain.Services/Services/ServiceMembers.cs
@@ -0,0 +1,12 @@
+﻿using CellManagerAPI.Domain.Core.Interfaces.Repositories;
+using CellManagerAPI.Domain.Core.Interfaces.Services;
+using CellManagerAPI.Domain.Models;
+
+namespace CellManagerAPI.Domain.Services.Services;
+
+public class ServiceMembers : ServiceBase<Member>, IServiceMembers
+{
+    public ServiceMembers(IRepositoryMembers repository) : base(repository)
+    {
+    }
+}
