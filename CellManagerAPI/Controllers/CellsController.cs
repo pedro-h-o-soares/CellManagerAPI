0d37583065886fa4b76b3bb579d0392be3cf3f1a
pedro-soares-fisarmonica <pedro.soares@fisarmonica.com.br>
2024-06-22 20:10:12 -0300

    Creating controllers

---
+++ CellManagerAPI/Controllers/CellsController.cs
@@ -0,0 +1,51 @@
+﻿using CellManagerAPI.Application.DTO.DTO;
+using CellManagerAPI.Application.Interfaces;
+using CellManagerAPI.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CellManagerAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CellsController : ControllerBase
+{
+    private readonly IApplicationServiceCells _service;
+
+    public CellsController(IApplicationServiceCells service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public IActionResult Get(
+        [FromQuery] int skip = 0,
+        [FromQuery] int take = 10)
+    {
+        return Ok(_service.GetAll(skip, take));
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var result = _service.GetById(id);
+        if (result is null) return NotFound();
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public IActionResult Create(CreateCellsDto dto)
+    {
+        try
+        {
+            ArgumentNullException.ThrowIfNull(dto);
+            var cell = _service.Add(dto);
+
+            return CreatedAtAction(nameof(Get), new { cell.Id }, cell);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
---
+++ CellManagerAPI/Controllers/SupervisionsController.cs
@@ -0,0 +1,50 @@
+﻿using CellManagerAPI.Application.DTO.DTO;
+using CellManagerAPI.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CellManagerAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SupervisionsController : ControllerBase
+{
+    private readonly IApplicationServiceSupervisions _service;
+
+    public SupervisionsController(IApplicationServiceSupervisions service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public IActionResult Get(
+        [FromQuery] int skip = 0,
+        [FromQuery] int take = 10)
+    {
+        return Ok(_service.GetAll(skip, take));
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var result = _service.GetById(id);
+        if (result is null) return NotFound();
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public IActionResult Create(CreateSupervisionsDto dto)
+    {
+        try
+        {
+            ArgumentNullException.ThrowIfNull(dto);
+            var supervision = _service.Add(dto);
+
+            return CreatedAtAction(nameof(Get), new { supervision.Id }, supervision);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
