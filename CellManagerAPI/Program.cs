1855588763cfe7c51fe9c4ce6c1cd33f890321af
pedro-h-o-soares <pedrosoares77744@gmail.com>
2024-06-22 20:10:12 -0300

    Creating controllers

--- CellManagerAPI/Program.cs
+++ CellManagerAPI/Program.cs
@@ -1,4 +1,11 @@
+using CellManagerAPI.Application.Interfaces;
+using CellManagerAPI.Application.Services;
+using CellManagerAPI.Domain.Core.Interfaces.Repositories;
+using CellManagerAPI.Domain.Core.Interfaces.Services;
+using CellManagerAPI.Domain.Services.Services;
+using CellManagerAPI.Infraestructure.CrossCutting.Adapter;
 using CellManagerAPI.Infraestructure.Data;
+using CellManagerAPI.Infraestructure.Repository.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +15,21 @@ var builder = WebApplication.CreateBuilder(args);
 var cellDbStrConn = builder.Configuration.GetConnectionString("CellDbConnection");
 
 builder.Services
-    .AddDbContext<CellManagerContext>(options =>
-        options.UseSqlServer(cellDbStrConn));
+    .AddDbContext<CellManagerContext>(opts =>
+        opts.UseLazyLoadingProxies()
+            .UseSqlServer(cellDbStrConn));
+
+Require.RequireAssembly();
+
+builder.Services.AddTransient<IApplicationServiceSupervisions, ApplicationServiceSupervisions>();
+builder.Services.AddTransient<IServiceSupervisions, ServiceSupervisions>();
+builder.Services.AddTransient<IRepositorySupervisions, RepositorySupervisions>();
+
+builder.Services.AddTransient<IApplicationServiceCells, ApplicationServiceCells>();
+builder.Services.AddTransient<IServiceCells, ServiceCells>();
+builder.Services.AddTransient<IRepositoryCells, RepositoryCells>();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
