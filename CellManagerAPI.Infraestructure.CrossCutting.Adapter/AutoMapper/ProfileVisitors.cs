5e6782fdab1dd612698a7160dc5c502c5e7b707e
pedro-soares-fisarmonica <pedro.soares@fisarmonica.com.br>
2024-06-22 20:09:12 -0300

    Creating automapper profiles

---
+++ CellManagerAPI.Infraestructure.CrossCutting.Adapter/AutoMapper/ProfileVisitors.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CellManagerAPI.Application.DTO.DTO;
+using CellManagerAPI.Domain.Models;
+
+namespace CellManagerAPI.Infraestructure.CrossCutting.Adapter.AutoMapper;
+
+public class ProfileVisitors : Profile
+{
+    public ProfileVisitors()
+    {
+        CreateMap<Visitor, ReadVisitorsDto>();
+        CreateMap<CreateVisitorsDto, Visitor>();
+    }
+}
