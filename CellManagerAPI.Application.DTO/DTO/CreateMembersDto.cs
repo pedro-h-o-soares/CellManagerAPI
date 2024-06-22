55612a026808a9e99ba6caec8af05e058c37e4fc
pedro-h-o-soares <pedrosoares77744@gmail.com>
2024-06-22 20:07:40 -0300

    Create DTOs

---
+++ CellManagerAPI.Application.DTO/DTO/CreateMembersDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CellManagerAPI.Application.DTO.DTO;
+
+public class CreateMembersDto : CreateBaseDto
+{
+    public string Name { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public int? CellId { get; set; }
+}
