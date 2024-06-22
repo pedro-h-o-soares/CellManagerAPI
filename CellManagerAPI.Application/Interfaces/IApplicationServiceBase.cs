c2f631a911fb9dff303aa2eb6601747706a66a2a
pedro-soares-fisarmonica <pedro.soares@fisarmonica.com.br>
2024-06-22 20:10:00 -0300

    Creating application services

---
+++ CellManagerAPI.Application/Interfaces/IApplicationServiceBase.cs
@@ -0,0 +1,22 @@
+﻿using CellManagerAPI.Application.DTO.DTO;
+using CellManagerAPI.Domain.Models;
+
+namespace CellManagerAPI.Application.Interfaces;
+
+public interface IApplicationServiceBase<TEntity, TCreateDto, TReadDto> : IDisposable
+    where TEntity  : Base
+    where TCreateDto : CreateBaseDto
+    where TReadDto : ReadBaseDto
+{
+    IEnumerable<TReadDto> GetAll(int skip, int take);
+    
+    TReadDto GetById(int id);
+
+    TEntity Add(TCreateDto dto);
+
+    void Update(int id, TCreateDto dto);
+
+    void Remove(int id);
+
+    void Dispose();
+}
---
+++ CellManagerAPI.Application/Interfaces/IApplicationServiceMembers.cs
@@ -0,0 +1,8 @@
+﻿using CellManagerAPI.Application.DTO.DTO;
+using CellManagerAPI.Domain.Models;
+
+namespace CellManagerAPI.Application.Interfaces;
+
+public interface IApplicationServiceMembers : IApplicationServiceBase<Member, CreateMembersDto, ReadMembersDto>
+{
+}
