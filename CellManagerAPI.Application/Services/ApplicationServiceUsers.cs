07eba468ca59eebf196d39446538f62b5e5004e9
pedro-h-o-soares <pedrosoares77744@gmail.com>
2024-07-23 20:59:11 -0300

    Create users endpoints

---
+++ CellManagerAPI.Application/Services/ApplicationServiceUsers.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using CellManagerAPI.Application.DTO.DTO;
+using CellManagerAPI.Application.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace CellManagerAPI.Application.Services;
+
+public class ApplicationServiceUsers : IApplicationServiceUsers
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly IMapper _mapper;
+
+    public ApplicationServiceUsers(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IMapper mapper)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _mapper = mapper;
+    }
+
+    public IEnumerable<ReadUsersDto> GetAll(int skip, int take)
+    {
+        var users = _userManager.Users.Skip(skip).Take(take).ToList();
+        return _mapper.Map<List<ReadUsersDto>>(users);
+    }
+
+    public async Task<ReadUsersDto> GetById(string id)
+    {
+        var user = _userManager.Users.FirstOrDefault(u => u.Id == id);
+        var dto = _mapper.Map<ReadUsersDto>(user);
+
+        dto.Roles = await _userManager.GetRolesAsync(user);
+
+        return dto;
+    }
+
+    public async Task Update(string id, UpdateUsersDto dto)
+    {
+        var obj = _userManager.Users.FirstOrDefault(u => u.Id == id) ?? throw new KeyNotFoundException($"No element with id [{id}] found");
+
+        _mapper.Map(dto, obj);
+
+        await _userManager.UpdateAsync(obj);
+    }
+
+    public async Task Patch(string id, JsonPatchDocument<UpdateUsersDto> patch)
+    {
+        var obj = _userManager.Users.FirstOrDefault(u => u.Id == id) ?? throw new KeyNotFoundException($"No element with id [{id}] found");
+        var entityPatch = _mapper.Map<JsonPatchDocument<IdentityUser>>(patch);
+
+        entityPatch.ApplyTo(obj);
+        await _userManager.UpdateAsync(obj);
+    }
+
+    public async Task Remove(string id)
+    {
+        var obj = _userManager.Users.FirstOrDefault(u => u.Id == id) ?? throw new KeyNotFoundException($"No element with id [{id}] found"); ;
+        await _userManager.DeleteAsync(obj);
+    }
+
+    public void Dispose()
+    {
+        _userManager.Dispose();
+    }
+}
---
+++ CellManagerAPI/Controllers/UsersController.cs
@@ -0,0 +1,95 @@
+﻿using CellManagerAPI.Application.DTO.DTO;
+using CellManagerAPI.Application.Interfaces;
+using CellManagerAPI.Domain.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CellManagerAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IApplicationServiceUsers _service;
+
+    public UsersController(IApplicationServiceUsers service)
+    {
+        _service = service;
+    }
+
+    /// <summary>
+    /// You can search for Users here.
+    /// </summary>
+    /// <param name="skip">Number of users to skip</param>
+    /// <param name="take">Number of users to take</param>
+    /// <returns>This endpoint returns a list of Users</returns>
+    [HttpGet]
+    public IActionResult Get(
+        [FromQuery] int skip = 0,
+        [FromQuery] int take = 10)
+    {
+        return Ok(_service.GetAll(skip, take));
+    }
+
+    /// <summary>
+    /// You can search for a specific User here.
+    /// </summary>
+    /// <param name="id">The id of the User you are looking for</param>
+    /// <returns>This endpoint returns the User with the given id</returns>
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(string id)
+    {
+        var result = await _service.GetById(id);
+        if (result is null) return NotFound();
+
+        return Ok(result);
+    }
+
+    /// <summary>
+    /// You can update Users here.
+    /// </summary>
+    /// <param name="id">Id of User to be updated</param>
+    /// <param name="dto">Object of User to be updated</param>
+    /// <returns>This endpoint returns the updated User</returns>
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(
+        string id,
+        [FromBody] UpdateUsersDto dto)
+    {
+        try
+        {
+            ArgumentNullException.ThrowIfNull(dto);
+            await _service.Update(id, dto);
+
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    /// <summary>
+    /// You can partially update Users here
+    /// </summary>
+    /// <param name="id">Id of User to be updated</param>
+    /// <param name="patch">JsonPatch object of User to be updated</param>
+    /// <returns>This endpoint returns no content</returns>
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> Patch(
+        string id,
+        [FromBody] JsonPatchDocument<UpdateUsersDto> patch)
+    {
+        try
+        {
+            ArgumentNullException.ThrowIfNull(patch);
+            await _service.Patch(id, patch);
+
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
